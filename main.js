d23fa53398a87eb4ae515622023a957334a1026a
BORNcode880 <donaldgjones880@gmail.com>
2023-05-19 11:27:32 -0700

    First commit, was able to get an output to display.

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-// Write your JavaScript code here!
+// Array of planet names and their respective weights
+var planets = [
+  ['Pluto', 0.06],
+  ['Neptune', 1.148],
+  ['Uranus', 0.917],
+  ['Saturn', 1.139],
+  ['Jupiter', 2.640],
+  ['Mars', 0.3895],
+  ['Moon', 0.1655],
+  ['Earth', 1],
+  ['Venus', 0.9032],
+  ['Mercury', 0.377],
+  ['Sun', 27.9]
+];
+
+// Calculate the weight on a specific planet
+function calculateWeight() {
+  var weight = parseFloat(document.getElementById('weight-input').value);
+  var planetIndex = document.getElementById('planet-select').selectedIndex;
+  
+  if (isNaN(weight)) {
+    document.getElementById('result-message').innerHTML = 'Please enter a valid weight.';
+  } else {
+    var planetWeight = weight * planets[planetIndex][1];
+    var planetName = planets[planetIndex][0];
+    var message = 'Your weight on ' + planetName + ' is ' + planetWeight.toFixed(2) + ' kg.';
+    document.getElementById('result-message').innerHTML = message;
+  }
+}
+
+document.getElementById('calculate-button').onclick = calculateWeight;
